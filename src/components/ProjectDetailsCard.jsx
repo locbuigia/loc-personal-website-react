14eb75b0aea6e4be7ccb71a420416a724726be1e
Loc Bui <locbuigia7101996@gmail.com>
2025-01-24 14:30:57 -0800

    Removed hover effect in ProjectDetailsCard

--- src/components/ProjectDetailsCard.jsx
+++ src/components/ProjectDetailsCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ProjectDetailsCard = ({ project }) => {
   return (
-    <div className="bg-gray-100 p-6 rounded shadow-lg flex flex-col justify-between items-center hover:-translate-x-1 hover:-translate-y-2 duration-300">
+    <div className="bg-gray-100 p-6 rounded shadow-lg flex flex-col justify-between items-center">
       <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
       <p className="text-gray-600 mb-6">{project.description}</p>
       <div className="space-x-4">
